696678192c72496fd5a2061ae03a4853bf513ae4
SrikanthBindela <srikanth.b@sincera.in>
2026-06-08 15:20:10 +0530

    KR ApplicationCustomizer

---
+++ src/extensions/bpclKrApplicationCustomizer/BpclKrApplicationCustomizerApplicationCustomizer.ts
@@ -0,0 +1,35 @@
+import { Log } from '@microsoft/sp-core-library';
+import {
+  BaseApplicationCustomizer
+} from '@microsoft/sp-application-base';
+import { Dialog } from '@microsoft/sp-dialog';
+
+import * as strings from 'BpclKrApplicationCustomizerApplicationCustomizerStrings';
+
+const LOG_SOURCE: string = 'BpclKrApplicationCustomizerApplicationCustomizer';
+
+
+export interface IBpclKrApplicationCustomizerApplicationCustomizerProperties {
+  // This is an example; replace with your own property
+  testMessage: string;
+}
+
+/** A Custom Action which can be run during execution of a Client Side Application */
+export default class BpclKrApplicationCustomizerApplicationCustomizer
+  extends BaseApplicationCustomizer<IBpclKrApplicationCustomizerApplicationCustomizerProperties> {
+
+  public onInit(): Promise<void> {
+    Log.info(LOG_SOURCE, `Initialized ${strings.Title}`);
+
+    let message: string = this.properties.testMessage;
+    if (!message) {
+      message = '(No properties were provided.)';
+    }
+
+    Dialog.alert(`Hello from ${strings.Title}:\n\n${message}`).catch(() => {
+      /* handle error */
+    });
+
+    return Promise.resolve();
+  }
+}
---
+++ src/extensions/bpclKrApplicationCustomizer/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IBpclKrApplicationCustomizerApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'BpclKrApplicationCustomizerApplicationCustomizerStrings' {
+  const strings: IBpclKrApplicationCustomizerApplicationCustomizerStrings;
+  export = strings;
+}
